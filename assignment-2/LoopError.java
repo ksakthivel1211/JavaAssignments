62d5c11169bdbf038786f7a7267a64c5830e750f
ksakthivel1211 <125632352+ksakthivel1211@users.noreply.github.com>
2023-03-16 19:19:26 +0530

    Update LoopError.java

--- assignment-2/LoopError.java
+++ assignment-2/LoopError.java
@@ -1,12 +1,11 @@
 package assignment2;
 import java.util.Scanner;
 import java.io.IOException;
-import java.math.*;
 
-class MathException extends Exception{
+class MathException extends NumberFormatException{
 	MathException()
 	{
-		System.out.println("Error: divided by zero");
+		System.out.println("Error: divided by zero or Negative number");
 	}
 }
 
@@ -16,34 +15,43 @@ public class LoopError {
 		Scanner sc = new Scanner(System.in);
 		String a=null,b=null;
 		while(true) {
+			try {
+				
 			System.out.println("Enter the numerator:");
-			a= sc.nextLine();
+			a= sc.next();
+			
 			if(a.startsWith("q") || a.startsWith("Q")) {
 				break;
 			}
-			System.out.println("Enter the denominator:");
-			b= sc.nextLine();
+			int c= Integer.parseInt(a);
 			
-			try {
-				int c= Integer.parseInt(a);
-				int d= Integer.parseInt(b);
-				
-				
+			System.out.println("Enter the denominator:");
+			b= sc.next();
+			
+			
+			int d= Integer.parseInt(b);
+			
 				if(d==0)
 				{
 					throw new MathException();
 					
+				}
+				else if(d<0 || c<0)
+				{
+					throw new MathException();
 				}
 				else
 				{
 					System.out.println(c/d);
 				}
 			}
-			catch(Exception e){
+			catch(MathException e) {}
+			catch(Exception e) {
 				System.out.println(e);
 			}
+			
 		}
 
 	}
+	}
 
-}
